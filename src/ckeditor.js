c67da362f03cb2df9d61e0ab01cd95511395740d
Sajal Khandelwal <khandelwal95.sajal@outlook.com>
2020-10-20 11:01:50 +0530

    Added Math Editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import MathType from '@wiris/mathtype-ckeditor5';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +57,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+    MathType
 ];
 
 // Editor configuration.
@@ -79,7 +81,9 @@ ClassicEditor.defaultConfig = {
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+            'MathType',
+            'ChemType'
 		]
 	},
 	image: {
